cc8cc5d4576c41da5e3fdeeb96d06877087a52a4
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-11-08 12:43:38 +0600

    blog page added

--- src/components/Reviews/AddReview.js
+++ src/components/Reviews/AddReview.js
@@ -2,9 +2,11 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/UserContext/UserContext';
 import { useForm } from 'react-hook-form';
 import swal from 'sweetalert';
+import { Link } from 'react-router-dom';
 
 const AddReview = ({ service }) => {
     const { _id } = service;
+
     const { user } = useContext(AuthContext);
     const userName = user?.displayName;
     const userEmail = user?.email;
@@ -43,23 +45,27 @@ const AddReview = ({ service }) => {
     }
     return (
         <div>
+            {
+                user ? <form onSubmit={handleSubmit} className='shadow-lg border w-[90%] mx-auto my-10 p-6 rounded'>
+                    <h1 className='text-center text-3xl font-bold'>Write A Review</h1>
 
-            <form onSubmit={handleSubmit} className='shadow-lg border w-[90%] mx-auto my-10 p-6 rounded'>
-                <h1 className='text-center text-3xl font-bold'>Write A Review</h1>
+                    <h2>Your Name</h2>
+                    <input type="text" name='name' required defaultValue={userName ? userName : 'Enter Your Name Here'} placeholder="Type here" className="input input-bordered input-secondary w-full max-w-xs" />
 
-                <h2>Your Name</h2>
-                <input type="text" name='name' required defaultValue={userName ? userName : 'Enter Your Name Here'} placeholder="Type here" className="input input-bordered input-secondary w-full max-w-xs" />
+                    <h2>Your Email</h2>
+                    <input name='email' type="email" required defaultValue={userEmail ? userEmail : 'Enter Your Email Here.'} placeholder="Type here" className="input input-bordered input-secondary w-full max-w-xs" />
 
-                <h2>Your Email</h2>
-                <input name='email' type="email" required defaultValue={userEmail ? userEmail : 'Enter Your Email Here.'} placeholder="Type here" className="input input-bordered input-secondary w-full max-w-xs" />
+                    <h2>Your Photo Link</h2>
+                    <input name='img' type="text" required defaultValue={userImg ? userImg : 'Enter Img link Here.'} placeholder="Type here" className="input input-bordered input-secondary w-full max-w-xs" />
 
-                <h2>Your Photo Link</h2>
-                <input name='img' type="text" required defaultValue={userImg ? userImg : 'Enter Img link Here.'} placeholder="Type here" className="input input-bordered input-secondary w-full max-w-xs" />
+                    <h2>Your Review</h2>
+                    <textarea name='review' className="textarea w-[95%] lg:w-[50%] textarea-secondary" placeholder="Your Review Here"></textarea> <br />
+                    <button type='submit' className='btn'>Add Review</button>
+                </form>
+                    :
+                    <h1 className='text-2xl text-center my-5'>Please Log in for Give a Review <Link to='/login' className='text-red-600 font-bold underline'>Log In</Link> </h1>
+            }
 
-                <h2>Your Review</h2>
-                <textarea name='review' className="textarea w-[95%] lg:w-[50%] textarea-secondary" placeholder="Your Review Here"></textarea> <br />
-                <button type='submit' className='btn'>Add Review</button>
-            </form>
         </div>
     );
 };
